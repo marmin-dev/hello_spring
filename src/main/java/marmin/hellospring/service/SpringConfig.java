9b97b6e20f20c10ba2081f5647dca2de47e009cf
Marmin <marmin.dev@gmail.com>
2022-09-14 03:00:54 +0900

    JavaBean by JavaCode

---
+++ src/main/java/marmin/hellospring/service/SpringConfig.java
@@ -0,0 +1,22 @@
+package marmin.hellospring.service;
+
+import marmin.hellospring.repository.MemberRepository;
+import marmin.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+
+}
